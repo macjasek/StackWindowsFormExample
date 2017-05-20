2cc72f75fcbeae68b71e4141ca1da99447e94da0
macjasek <maciej.dobruchowski@ispro.com.pl>
2017-05-20 16:28:20 +0200

    Import Project

---
+++ WindowsFormsStackExample/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsStackExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPush = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pushValue = new System.Windows.Forms.TextBox();
+            this.labelPush = new System.Windows.Forms.Label();
+            this.btnPushOK = new System.Windows.Forms.Button();
+            this.btnPop = new System.Windows.Forms.Button();
+            this.btnPeek = new System.Windows.Forms.Button();
+            this.btnViewAll = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPush
+            // 
+            this.btnPush.Location = new System.Drawing.Point(12, 49);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(75, 23);
+            this.btnPush.TabIndex = 0;
+            this.btnPush.Text = "Push";
+            this.btnPush.UseVisualStyleBackColor = true;
+            this.btnPush.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Wybierz opcje";
+            // 
+            // pushValue
+            // 
+            this.pushValue.Location = new System.Drawing.Point(105, 87);
+            this.pushValue.Name = "pushValue";
+            this.pushValue.Size = new System.Drawing.Size(308, 20);
+            this.pushValue.TabIndex = 2;
+            this.pushValue.Visible = false;
+            // 
+            // labelPush
+            // 
+            this.labelPush.AutoSize = true;
+            this.labelPush.Location = new System.Drawing.Point(12, 94);
+            this.labelPush.Name = "labelPush";
+            this.labelPush.Size = new System.Drawing.Size(76, 13);
+            this.labelPush.TabIndex = 3;
+            this.labelPush.Text = "Wpisz wartość";
+            this.labelPush.Visible = false;
+            // 
+            // btnPushOK
+            // 
+            this.btnPushOK.Location = new System.Drawing.Point(338, 113);
+            this.btnPushOK.Name = "btnPushOK";
+            this.btnPushOK.Size = new System.Drawing.Size(75, 23);
+            this.btnPushOK.TabIndex = 4;
+            this.btnPushOK.Text = "OK";
+            this.btnPushOK.UseVisualStyleBackColor = true;
+            this.btnPushOK.Visible = false;
+            this.btnPushOK.Click += new System.EventHandler(this.btnPushOK_Click);
+            // 
+            // btnPop
+            // 
+            this.btnPop.Location = new System.Drawing.Point(94, 49);
+            this.btnPop.Name = "btnPop";
+            this.btnPop.Size = new System.Drawing.Size(75, 23);
+            this.btnPop.TabIndex = 5;
+            this.btnPop.Text = "Pop";
+            this.btnPop.UseVisualStyleBackColor = true;
+            this.btnPop.Click += new System.EventHandler(this.btnPop_Click);
+            // 
+            // btnPeek
+            // 
+            this.btnPeek.Location = new System.Drawing.Point(175, 49);
+            this.btnPeek.Name = "btnPeek";
+            this.btnPeek.Size = new System.Drawing.Size(75, 23);
+            this.btnPeek.TabIndex = 6;
+            this.btnPeek.Text = "Peek";
+            this.btnPeek.UseVisualStyleBackColor = true;
+            this.btnPeek.Click += new System.EventHandler(this.btnPeek_Click);
+            // 
+            // btnViewAll
+            // 
+            this.btnViewAll.Location = new System.Drawing.Point(256, 49);
+            this.btnViewAll.Name = "btnViewAll";
+            this.btnViewAll.Size = new System.Drawing.Size(75, 23);
+            this.btnViewAll.TabIndex = 7;
+            this.btnViewAll.Text = "View All";
+            this.btnViewAll.UseVisualStyleBackColor = true;
+            this.btnViewAll.Click += new System.EventHandler(this.btnViewAll_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(338, 48);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 148);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnViewAll);
+            this.Controls.Add(this.btnPeek);
+            this.Controls.Add(this.btnPop);
+            this.Controls.Add(this.btnPushOK);
+            this.Controls.Add(this.labelPush);
+            this.Controls.Add(this.pushValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPush);
+            this.Name = "Form1";
+            this.Text = "Stack Windows Form Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPush;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pushValue;
+        private System.Windows.Forms.Label labelPush;
+        private System.Windows.Forms.Button btnPushOK;
+        private System.Windows.Forms.Button btnPop;
+        private System.Windows.Forms.Button btnPeek;
+        private System.Windows.Forms.Button btnViewAll;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ WindowsFormsStackExample/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsStackExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PushVisibility(true);
+        }
+
+        private void PushVisibility(bool vis)
+        {
+            btnPushOK.Visible = vis;
+            labelPush.Visible = vis;
+            pushValue.Visible = vis;
+        }
+
+        private void btnPushOK_Click(object sender, EventArgs e)
+        {
+            if (pushValue.Text != "")
+            {
+                MyStack._myStack.Push(pushValue.Text);
+                pushValue.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Błąd. Nie wpisano żadnej wartości!");
+            }
+            PushVisibility(false);
+        }
+
+        private void btnPop_Click(object sender, EventArgs e)
+        {
+            if (MyStack._myStack.Count > 0)
+            {
+                var stackElement = MyStack._myStack.Pop();
+                MessageBox.Show($"Usunięto ze stosu element {stackElement}");
+            }
+            else
+            {
+                MessageBox.Show("Błąd. Stos pusty nie można usunąć elementu");
+            }
+        }
+
+        private void btnPeek_Click(object sender, EventArgs e)
+        {
+            if (MyStack._myStack.Count > 0)
+            {
+                MessageBox.Show($"Element na górze stosu: {MyStack._myStack.Peek()}");
+            }
+            else
+            {
+                MessageBox.Show("Stos pusty.");
+            }
+        }
+
+        private void btnViewAll_Click(object sender, EventArgs e)
+        {
+            var stackList = string.Join("\n", MyStack._myStack);
+            MessageBox.Show(stackList, "Lista elementów na stosie");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
